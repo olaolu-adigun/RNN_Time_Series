f4d288e776a17b7d7cee956d6d932c3b72028d99
ADIGUN OLAOLUWA <olaolu_adigun@yahoo.com>
2016-10-26 00:12:55 -0700

    commit 3

--- Backpropagation.m
+++ Backpropagation.m
@@ -1,7 +1,9 @@
 function [delW1, delW2, delW3, del_bias1, del_bias2, del_bias3] = Backpropagation(OH, AH, OY,AY, ax, ay, net)
 
-  
- for  i = 1:1:N
+ delW3 = zeros(size(net.W3));
+ for  i = 1:1:size(ax,1)
+     %---Compute change for W1
+     del
      
  end
 
