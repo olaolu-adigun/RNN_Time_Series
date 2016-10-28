ecaa221d3463bd513b1894130315f06ba000e667
ADIGUN OLAOLUWA <olaolu_adigun@yahoo.com>
2016-10-27 23:41:36 -0700

    commit 1

--- main.m
+++ main.m
@@ -74,6 +74,5 @@ for iter = 1:1:opts.numepochs
         
         [OH, AH, OY, AY] = Forward(ax, nn);
         [delW1, delW2, delW3, del_bias1, del_bias2, del_bias3] = Backpropagation(OH, AH, OY,AY, ax, ay, nn);
-    
     end
 end
